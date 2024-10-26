929266892f5a2682c37251758d191e139662db93
AnandK-pm <pmanandkrishnan@gmail.com>
2024-10-27 01:05:05 +0530

    added a go to the top button

--- quikread/quikread/settings.py
+++ quikread/quikread/settings.py
@@ -135,7 +135,7 @@ CELERY_BEAT_SCHEDULE = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = "static_files/"
+STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     BASE_DIR/ "static"
 ]
